58e173d65db392b281d8dec49fcec3ea13c9b43a
Sheraji <???-github-email@example.com>
2026-08-31 22:52:34 +0600

    Initial setup: Firebase config + project structure

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBXKvuDqiTQJ2KuN78_uvbRwUFTS8g2ts4",
+  authDomain: "ahc-gm.firebaseapp.com",
+  projectId: "ahc-gm",
+  storageBucket: "ahc-gm.firebasestorage.app",
+  messagingSenderId: "313727793149",
+  appId: "1:313727793149:web:cf1c23278c4f08fdb14b94"
+};
+
+export default firebaseConfig;
